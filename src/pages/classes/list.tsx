4af28eb12aa60a0bc5e5382d61597a055273af05
EDubov <Edubov@project.ibs.ru>
2026-04-02 17:26:01 +0500

    [WIP]: add upload widget and create form

---
+++ src/pages/classes/list.tsx
@@ -0,0 +1,11 @@
+import { CreateButton } from "@/components/refine-ui/buttons/create";
+
+const List = () => {
+  return (
+    <div>
+      <CreateButton />
+    </div>
+  );
+};
+
+export default List;
