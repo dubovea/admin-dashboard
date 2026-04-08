dce0e1e4a77081af6af1e73e733892fba30b6f5c
EDubov <Edubov@project.ibs.ru>
2026-04-08 18:54:31 +0500

    implement show.tsx and cloudinary img generator

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,5 @@
 import { BACKEND_BASE_URL } from "@/assets/constants";
-import { CreateResponse, ListResponse } from "@/types";
+import { CreateResponse, GetOneResponse, ListResponse } from "@/types";
 import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 
@@ -70,6 +70,13 @@ const options: CreateDataProviderOptions = {
     buildBodyParams: async ({ variables }) => variables,
     mapResponse: async (response) => {
       const json: CreateResponse = await response.json();
+      return json?.data ?? [];
+    },
+  },
+  getOne: {
+    getEndpoint: ({ resource, id }) => `${resource}/${id}`,
+    mapResponse: async (response) => {
+      const json: GetOneResponse = await response.json();
       return json?.data ?? [];
     },
   },
